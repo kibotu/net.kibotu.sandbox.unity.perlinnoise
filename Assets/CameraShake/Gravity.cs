992801f811b2f7a50db164f3acd29c8d2416c3c5
Jan Rabe <jan.rabe@wooga.net>
2014-09-18 20:48:18 +0200

    added camera shake scene

---
+++ Assets/CameraShake/Gravity.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	    rigidbody.AddForce(new Vector3(Input.acceleration.x, Input.acceleration.y,0 ));
+	}
+}
